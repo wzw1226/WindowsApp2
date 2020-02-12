235ca1edf54b74227705bca3f30d696e856310bb
wzw1226 <1518787048@qq.com>
2020-02-12 23:47:12 +0800

    SETUP

---
+++ WindowsApp2/LoginForm1.vb
@@ -0,0 +1,19 @@
+Public Class LoginForm1
+
+    ' TODO: 插入代码，以使用提供的用户名和密码执行自定义的身份验证
+    ' (请参阅 https://go.microsoft.com/fwlink/?LinkId=35339)。  
+    ' 随后自定义主体可附加到当前线程的主体，如下所示: 
+    '     My.User.CurrentPrincipal = CustomPrincipal
+    ' 其中 CustomPrincipal 是用于执行身份验证的 IPrincipal 实现。
+    ' 随后，My.User 将返回 CustomPrincipal 对象中封装的标识信息
+    ' 如用户名、显示名等
+
+    Private Sub OK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Cancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel.Click
+        Me.Close()
+    End Sub
+
+End Class
